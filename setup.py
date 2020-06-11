d8467ed25b8be0309fe764f6c41b87ec88230184
David Berthel <gitlab@davidberthel.de>
2020-06-11 01:03:50 +0000

    Changed name of package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='archaeomat.meshlabxml',
+setup(name='archaeomat-utils-meshlabxml',
       version='2018.3',
       description='Create and run MeshLab XML scripts',
       long_description=long_description,
