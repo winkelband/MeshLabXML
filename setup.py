4e1f833cb81f1014a74ae3280f524b62e63cd2d7
David Berthel <gitlab@davidberthel.de>
2020-06-11 00:49:44 +0000

    Changed namespace → archaeomat.meshlabxml

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(name='archaeomat.meshlabxml',
       author='3DLirious, LLC',
       author_email='3DLirious@gmail.com',
       license='LGPL-2.1',
-      packages=['meshlabxml'],
+      packages=['archaeomat.meshlabxml'],
       include_package_data=True)
